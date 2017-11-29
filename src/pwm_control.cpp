b5becbd254c9374519a198180ee48d1466d1f4e1
pontuspg <pettersson-gull@hotmail.com>
2017-11-29 08:47:58 +0000

    Balancing receive added

--- src/pwm_control.cpp
+++ src/pwm_control.cpp
@@ -19,7 +19,10 @@
 #define PPM 	4
 #define SOFTPWM 15
 #define WALKASSIST 23
-#define SPEEDOMETER 14
+#define SPEEDOMETER 26
+
+
+#define PI 3.141592
 
 int vel_data = 0;
 
@@ -44,11 +47,11 @@ unsigned int deltaTime;
 
 //Time variables for steering motor
 unsigned int previousTick_angular;
-unsigned int deltaTime_angular;
+float deltaTime_angular;
 
 //Time variables for driving motor
 unsigned int previousTick_linear;
-unsigned int deltaTime_linear;
+float deltaTime_linear;
 
 float frequencyDriveMotor;
 
@@ -61,7 +64,9 @@ float angularSpeed = 0;
 float linearSpeed = 0;
 
 
+float r = 0.36;
 
+int balance = 0;
 
 //Exit routine
 void sigintHandler (int sig){
@@ -78,13 +83,17 @@ void sigintHandler (int sig){
 	ros::shutdown();
 }
 
-void speedometerInterrupt(int gpio, int level, uint32_t tick){
 
-	if(level == 1){
+//Velocity of the bike. Since the sensor on the bike is not working this function is not used at the moment
+void speedometerInterrupt(int gpio, int level, uint32_t tick){
+	
+	if(level == 0 && (tick-previousTick_linear) > 100000){
 		
 		deltaTime_linear = tick-previousTick_linear;
-		frequencyDriveMotor = 1/(deltaTime/1000000);
-		linearSpeed = 0.3566*frequencyDriveMotor-0.002715;
+
+		ROS_INFO("low [%f]",deltaTime_linear);
+		linearSpeed = 2*PI*r/(deltaTime_linear/1000000);
+		
 		previousTick_linear = tick;
 	}
 
@@ -104,7 +113,8 @@ void interruptPPM(int gpio, int level, uint32_t tick){
 				//Check left stick, values above 1510 will output as pointing to the right
 				//values below 1490 will output as pointing to the left
 				//deltaTime around 1500 is when the stick is in the middle, thus comparing to 					//1510 and 1490
-				if(channels[3] > 1530 && stop == 0){									     ROS_INFO("[%lf]",currentAngle);
+				if(channels[3] > 1530 && stop == 0 && balance == 0){									    
+				       	
 					//Right		
 					stickVal = abs(channels[3]-1530);
 					gpioWrite(DIR,1);
@@ -113,9 +123,8 @@ void interruptPPM(int gpio, int level, uint32_t tick){
 					}else{
 						gpioHardwarePWM(GPIO,50000,stickVal*500);
 					}
-				}else if(channels[3] < 1470 && stop == 0){	
+				}else if(channels[3] < 1470 && stop == 0 && balance == 0){	
 					//Left		
-					ROS_INFO("[%lf]",currentAngle);	
 					stickVal = abs(channels[3]-1470);
 					gpioWrite(DIR,0);
 					if(currentAngle < -90){
@@ -130,20 +139,21 @@ void interruptPPM(int gpio, int level, uint32_t tick){
 				//values below 1490 will output as pointing down		
 				//deltaTime 1500 = stick is in middlepoint		
 				if(channels[1] < 1490){			
-					//Down		
+					//Down, motor off	
 					gpioPWM(SOFTPWM,0);
 					
 				}else if(channels[1] > 1510 && stop == 0){		
-					//Up			
+					//Up, motor on		
 					gpioPWM(SOFTPWM,128);
 					
 				}
 				
-				//SWA lever on the controller, up is on down is off
+				//SWA lever on the controller, up is on down is off, currently used to stop all motors
 				if(channels[4] < 1500){
 					//Motors OFF
 					gpioPWM(SOFTPWM,0);
 					gpioHardwarePWM(GPIO,50000,0);
+					
 					stop = 1;
 					
 					
@@ -152,7 +162,7 @@ void interruptPPM(int gpio, int level, uint32_t tick){
 					stop = 0;
 				}
 				
-				//SWB lever on the controller, up is 0 middle is 1 and down is 2
+				//SWB lever on the controller, not currently in use
 				if(channels[5] < 1200){
 					
 				}else if (channels[5] > 1200 && channels[5] < 1700){
@@ -161,20 +171,20 @@ void interruptPPM(int gpio, int level, uint32_t tick){
 					
 				}
 				
-				//SWC lever on the controller, up is 0 middle is 1 and down is 2
-				if(channels[6] < 1200){
-					
+				//SWC lever on the controller, not currently in use
+				if(channels[6] < 1200 || stop == 1){
+					balance = 0;
 				}else if (channels[6] > 1200 && channels[6] < 1700){
 					
 				}else{
-					
+					balance = 1;	
 				}		
 				
-				//SWD lever on the controller, up is 0 down is 1
-				if(channels[7] < 1500){
-					gpioWrite(WALKASSIST,1);
-				}else{
+				//SWD lever on the controller, up activates the walk assist mode (slowest speed)
+				if(channels[7] > 1500 && stop == 0){
 					gpioWrite(WALKASSIST,0);
+				}else{
+					gpioWrite(WALKASSIST,1);
 				}			
 		}
 		//If between to sync pulses we fill the channel array with values for each channel
@@ -191,27 +201,36 @@ void interruptPPM(int gpio, int level, uint32_t tick){
 void encoderInterruptA(int gpio, int level, uint32_t tick){
 	encoderA = level;
 	
-	//deltaTime_angular = tick-previousTick_angular;
-
+	
 	//When encoder a has a rising edge encoder B will be low if the motor is rotating clockwise
 	//If the motor is rotating counter clockwise channel B will be high when channel A has a 	  
 	//rising edge
 	if(encoderA == 1 && encoderB == 0){
 		CW = 1;
 		//Adding the angle of one pulse in the direction the motor is rotating, (360/PPR = angle of one pulse)
-	
+		deltaTime_angular = tick-previousTick_angular;
+		
+		//Adding the angle of one pulse in the direction the motor is rotating	
 		currentAngle = currentAngle + (360/PPR);
-		//angularSpeed = (360/PPR)/(deltaTime_angular/1000000);
+		
+
+		//Calculate angular velocity
+		angularSpeed = (360/PPR)/(deltaTime_angular/1000000);
+		
+		//Get new latest tick time
+		previousTick_angular = tick;
 	}else if(encoderA == 1 && encoderB == 1){
 		CW = 0;
+		deltaTime_angular = tick-previousTick_angular;
 		//Adding the angle of one pulse in the direction the motor is rotating
 		currentAngle = currentAngle - (360/PPR);
-		//angularSpeed = -(360/PPR)/(deltaTime_angular/1000000);
+		//Calculate angular velocity
+		angularSpeed = -(360/PPR)/(deltaTime_angular/1000000);
+		
+		//Get new latest tick time
+		previousTick_angular = tick;
 	}
 	
-
-
-	//previousTick_angular = tick;	
 }
 
 //Recieve encoder data from channel B. Enters when the level changes on the GPIO pin
@@ -221,18 +240,18 @@ void encoderInterruptB(int gpio, int level, uint32_t tick){
 }
 
 
-//Controlling the motor through a ROS topic
+//Controlling the motor through a ROS topic, used for testing
 void velCallback(const geometry_msgs::Twist::ConstPtr& vel) {
 	vel_data  = vel->linear.x;
 	ROS_INFO("velCallback");
 	switch(vel_data){
 	case 1:
 		if(speed < 1000000){
-			speed = speed+100000;
+			speed = speed+30000;
 			if(speed < 0){
-				gpioWrite(DIR,1);
-			}else{
 				gpioWrite(DIR,0);
+			}else{
+				gpioWrite(DIR,1);
 			}
 			gpioHardwarePWM(GPIO,50000,abs(speed));
 			printf("Duty cycle increased to: [%f] \n",(float)gpioGetPWMdutycycle(18)/1000000);
@@ -246,11 +265,11 @@ void velCallback(const geometry_msgs::Twist::ConstPtr& vel) {
 		break;
 	case 3:
 		if(speed > -1000000){
-			speed = speed-100000;
+			speed = speed-30000;
 			if(speed < 0){
-				gpioWrite(DIR,1);
-			}else{
 				gpioWrite(DIR,0);
+			}else{
+				gpioWrite(DIR,1);
 			}
 			gpioHardwarePWM(GPIO,50000,abs(speed));
 			printf("Duty cycle decreased to: [%f] \n",(float)gpioGetPWMdutycycle(18)/1000000);
@@ -266,7 +285,7 @@ void velCallback(const geometry_msgs::Twist::ConstPtr& vel) {
 		//Negative or positive angle, i.e direction of the motor
 		
 		if(currentAngle < vel->linear.y){
-			gpioWrite(DIR,0);
+			gpioWrite(DIR,1);
 			ROS_INFO("CW");
 			while(currentAngle <= vel->linear.y){	
 				if(stop == 1){
@@ -275,7 +294,7 @@ void velCallback(const geometry_msgs::Twist::ConstPtr& vel) {
 			}
 			
 		}else if(currentAngle  > vel->linear.y){
-			gpioWrite(DIR,1);
+			gpioWrite(DIR,0);
 			ROS_INFO("CCW");
 			while(currentAngle >= vel->linear.y){
 				if(stop == 1){
@@ -293,6 +312,37 @@ void velCallback(const geometry_msgs::Twist::ConstPtr& vel) {
 	}
 }
 
+// Function to set velocity of steering motor sent from balancing algorithm
+void controlCallback(const std_msgs::Float32 velMotor){
+
+	if(balance == 1){		
+			
+		//Calculate duty cycle to get correct speed
+		speed = (7*velMotor.data*(180/PI))/32+5/4;
+	
+		//set motor to the correct direction
+		if(velMotor.data > 0.0){
+			gpioWrite(DIR,1);
+			CW = 1;
+
+		//If velMotor is negative change direction	
+		}else if(velMotor.data < 0.0){		
+			gpioWrite(DIR,0);
+			CW = 0;
+		}
+
+		//Set PWM
+		if(CW == 1 && currentAngle > 90){
+			gpioHardwarePWM(GPIO,50000,speed*0);
+		}else if(CW == 0 && currentAngle < -90){
+			gpioHardwarePWM(GPIO,50000,speed*0);
+		}else{
+			gpioHardwarePWM(GPIO,50000,abs(speed*10000));
+		}
+	}
+
+}
+
 
 int main (int argc, char **argv)
 {
@@ -303,6 +353,7 @@ int main (int argc, char **argv)
   
   geometry_msgs::Twist speed_msg;
   std_msgs::Float32 rotation_msg;
+
   signal (SIGINT, sigintHandler);
   ros::Rate rate(100);  // hz
   
@@ -325,40 +376,52 @@ gpioCfgClock(1,1,0);
 //Set gpio mode for the needed GPIO pins
 gpioSetMode(SOFTPWM,PI_OUTPUT);
 gpioSetMode(GPIO,PI_OUTPUT);
-//gpioSetMode(PPM,PI_INPUT);
+gpioSetMode(PPM,PI_INPUT);
 gpioSetMode(ENCODERA,PI_INPUT);
 gpioSetMode(ENCODERB,PI_INPUT);
-//gpioSetMode(WALKASSIST,PI_OUTPUT);
+gpioSetMode(WALKASSIST,PI_OUTPUT);
 gpioSetMode(DIR,PI_OUTPUT);
 gpioSetMode(15,PI_OUTPUT);
-//gpioSetMode(SPEEDOMETER,PI_INPUT);
+gpioSetMode(SPEEDOMETER,PI_INPUT);
 
-gpioWrite(DIR,turn);
-//gpioWrite(WALKASSIST,1);
-//gpioSetPWMfrequency(SOFTPWM,10);
+
+gpioWrite(DIR,0);
+gpioWrite(WALKASSIST,1);
+//set frequency of the softpwm used for triggering the front wheel (not walk assist mode)
+gpioSetPWMfrequency(SOFTPWM,10);
 
 //Functions for recieving encoder data
 gpioSetAlertFunc(ENCODERA, encoderInterruptA);
 gpioSetAlertFunc(ENCODERB, encoderInterruptB);
-//gpioSetAlertFunc(SPEEDOMETER, speedometerInterrupt);
+
+//activate this if sensor on the front wheel are working
+gpioSetAlertFunc(SPEEDOMETER, speedometerInterrupt);
 
 gpioSetAlertFunc(PPM, interruptPPM);
 
+//Topic to publish speed of the steering motor (linear.x) and the front wheel (angular.x)
 ros::Publisher pub_speed = node.advertise<geometry_msgs::Twist>("speed_topic", 10);
+
+//Topic to publish the current angle of the steering motor
 ros::Publisher pub_rotation = node.advertise<std_msgs::Float32>("rotation_topic",10);
 
+
+ros::Subscriber vel_sub = node.subscribe("sender_topic",10, velCallback);
+ros::Subscriber control_sub = node.subscribe("control",10,controlCallback);
+
 while(ros::ok()) {
-	ros::Subscriber vel_sub = node.subscribe("sender_topic",10, velCallback);
+	
 	//ROS_INFO("[%lf]",currentAngle);	
 	rotation_msg.data = currentAngle;
   	pub_rotation.publish(rotation_msg);
-
+	
+	
 	speed_msg.linear.x = linearSpeed;
 	speed_msg.angular.x = angularSpeed;
 
 	pub_speed.publish(speed_msg);
 	ros::spinOnce();
-	
+	rate.sleep();
 }
 return 0;
 }
